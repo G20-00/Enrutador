a4f386a6118cbd8d0d507b6c632b590f3e58eda1
Gianni <70019070+G20-00@users.noreply.github.com>
2022-02-12 20:07:03 -0500

    add

--- script.js
+++ script.js
@@ -7,18 +7,19 @@ function home() {
     let link = document.createElement('a');
     link.href = '#/about';
     link.innerText = 'About';
-
+    
     div.innerHTML = '<h1>Home</h1>';
     div.appendChild(link);
 
     app_div.appendChild(div);
+   
 };
 function about() {
     let div = document.createElement('div');
     let link = document.createElement('a');
     link.href = '#/';
     link.innerText = 'Home';
-
+    
     div.innerHTML = '<h1>About</h1>';
     div.appendChild(link);
 
@@ -26,28 +27,37 @@ function about() {
 };
 function route (path, template) {
     if (typeof template === 'function') {
+        
         return routes[path] = template;
     }
     else if (typeof template === 'string') {
+        
         return routes[path] = templates[template];
     } else {
+        
         return;
     };
 };
 function template (name, templateFunction) {
+    
     return templates[name] = templateFunction;
 };
 template('home', function(){
+    
     home();
+    
 });
 
 template('about', function(){
+    
     about();
 });
 route('/', 'home');
 route('/about', 'about');
+
 function resolveRoute(route) {
     try {
+        
         return routes[route];
     } catch (e) {
         throw new Error(`Route ${route} not found`);
